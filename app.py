6137dfa22727fad0a227f273df993f4420f1f1c3
nikhilthakur258 <nikhilthakur258@gmail.com>
2023-12-05 11:11:12 -0500

    GenAI

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, jsonify
+import subprocess
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/Generate_Requirements')
+def Test():
+    return render_template('Generate_Requirements.html')
+
+@app.route('/run_script', methods=['POST'])
+def run_script():
+    try:
+        data = request.get_json()
+        script_path = os.path.join('scripts', data['scriptPath'])  # Adjust the path
+        input_text = data['inputText']
+
+        # Run the Python script using subprocess
+        result = subprocess.check_output(['python', script_path, input_text], text=True)
+
+        return jsonify(success=True, output=result)
+    except subprocess.CalledProcessError as e:
+        print(f"CalledProcessError: {e}")
+        print(f"Output: {e.output}")
+        return jsonify(success=False, error=e.output)
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        return jsonify(success=False, error=str(e))
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ scripts/AzureOpenAI.py
@@ -0,0 +1,40 @@
+import openai
+import argparse
+
+# Set up OpenAI API key
+openai.api_key = '7f66b5661345437d80e661020d74a2c9'
+openai.api_base = 'https://genaiusecases.openai.azure.com/'
+openai.api_type = 'azure'
+openai.api_version = '2023-07-01-preview'  # this may change in the future
+deployment_name = 'OpenAIPOC'
+output_file = "Requirement_UI.txt"
+
+
+# Function to generate test cases using OpenAI GPT-3
+def generate_test_cases(description_to_append):
+    prompt = description_to_append
+    response = openai.Completion.create(engine=deployment_name, prompt=prompt, temperature=0.0, max_tokens=2000,
+                                        top_p=0.95, frequency_penalty=0, presence_penalty=0, stop=None)
+    for testcase in response["choices"]:
+        print(f"{testcase['text']}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Transform requirements into Gherkins")
+    parser.add_argument("cleaned_description", help="Cleaned description to use for generating test cases.")
+    
+    args = parser.parse_args()
+
+    try:
+        # Use provided cleaned_description
+        cleaned_description = "Write code in Java selenium to test below scenario: \n" + args.cleaned_description
+
+        # Append to file
+        with open(output_file, 'w') as file:
+            file.write(cleaned_description)
+
+        # Generate test cases
+        generated_test_cases = generate_test_cases(cleaned_description)
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
